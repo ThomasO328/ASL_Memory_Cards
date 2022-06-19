b9dd5af7428700672f0d3794807dd33a716422cd
ThomasO328 <100553765+ThomasO328@users.noreply.github.com>
2022-06-19 13:13:22 -0700

    prep work started

---
+++ script.js
@@ -0,0 +1,19 @@
+//score tracker
+
+//start game
+
+//countdown starts
+
+//flip cards
+
+//count flips
+
+//shuffle cards
+
+//flip 2 check for match
+
+//return facedown if no match
+
+//win screen after matching all
+
+//lose screen if time runs out
