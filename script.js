d4ba51fe44f7a416740ffacbd5acc72f31a4b592
ThomasO328 <100553765+ThomasO328@users.noreply.github.com>
2022-06-19 18:05:48 -0700

    worked in JS. Game is operational

--- script.js
+++ script.js
@@ -1,19 +1,143 @@
-//score tracker
+class totalScore{
+    constructor(totalTime, cards){
+        this.cardsArray = cards;
+        this.totalTime = totalTime;
+        this.timeRemaining = totalTime;
+        this.timer = document.getElementById('time-remaining');
+        this.ticker = document.getElementById('flips');
+    }
+    // start timer, flip count, shuffle card
+    startGame() {
+        this.cardToCheck = null;
+        this.totalClicks = 0;
+        this.timeRemaining = this.totalTime;
+        this.matchedCards = [];
+        this.busy = true;
+        setTimeout(() => {
+            this.shuffleCards();
+            this.countDown = this.startCountDown();
+            this.busy = false;
+        }, 500);
+        this.hideCards();
+        this.timer.innerText = this.timeRemaining;
+        this.ticker.innerText = this.totalClicks;
+    }
+    //flip cards down starting new game
+    hideCards() {
+        this.cardsArray.forEach(card => {
+            card.classList.remove('visible');
+            card.classList.remove('matched');
+        });
+    }
 
-//start game
+    flipCard(card){
+        if(this.canFlipCard(card)){
+           this.totalClicks++;
+           this.ticker.innerText = this.totalClicks; 
+           card.classList.add('visible')
 
-//countdown starts
+           //if statement
+           if(this.cardToCheck)
+                this.checkForCardMatch(card);
+           //check for match
+           else
+                this.cardToCheck = card;
+        
+        }
+    }
+    //check for card match
+    checkForCardMatch(card) {
+        if(this.getCardType(card) === this.getCardType(this.cardToCheck))
+            this.cardMatch(card, this.cardToCheck);
+        else
+            this.cardMisMatch(card, this.cardToCheck);
 
-//flip cards
+        this.cardToCheck = null;
+    }
+    //keep cards face up if match
+    cardMatch(card1, card2) {
+        this.matchedCards.push(card1);
+        this.matchedCards.push(card1);
+        card1.classList.add('matched');
+        card2.classList.add('matched');
+        if(this.matchedCards.length === this.cardsArray.length)
+            this.youWin();
+    }
+    //flip cards back down if not a match
+    cardMisMatch(card1, card2) {
+        this.busy = true;
+        setTimeout(() => {
+            card1.classList.remove('visible');
+            card2.classList.remove('visible');
+            this.busy = false;
+        }, 1500);
+    }
+    getCardType(card) {
+        return card.getElementsByClassName('card-value')[0].src;
+    }
+    //Game over screen when time runs out
+    startCountDown(){
+        return setInterval(() => {
+            this.timeRemaining--;
+            this.timer.innerText = this.timeRemaining;
+            if(this.timeRemaining === 0)
+                this.gameOver();
+        }, 1000);
+    }
+    gameOver() {
+        clearInterval(this.countDown);
+        document.getElementById('game-over').classList.add('visible');
+    }
+    youWin() {
+        clearInterval(this.countDown);
+        document.getElementById('you-win').classList.add('visible');
+    }
+    // shuffle cards
+    shuffleCards(){
+        for(let i = this.cardsArray.length - 1; i> 0; i--) {
+            let randomIndex = Math.floor(Math.random() * (i+1));
+            this.cardsArray[randomIndex].style.order = i;
+            this.cardsArray[i].style.order = randomIndex;
+        }
+    }
 
-//count flips
 
-//shuffle cards
+    canFlipCard(card) {
+        return !this.busy && !this.matchedCards.includes(card) && card !== this.cardToCheck;
+    }
+}
 
-//flip 2 check for match
+function ready() {
+    let overlays = Array.from(document.getElementsByClassName('overlay-text'));
+    let cards = Array.from(document.getElementsByClassName('card'));
+    let game = new totalScore(120, cards)
 
-//return facedown if no match
+    overlays.forEach(overlay => {
+        overlay.addEventListener('click', () => {
+            overlay.classList.remove('visible');
+             game.startGame();
+        });
+    });
+    cards.forEach(card => {
+        card.addEventListener('click', () => {
+            game.flipCard(card);
+        });
+    });
+}
 
-//win screen after matching all
+if(document.readyState === 'loading'){
+    document.addEventListener('DOMContentLoaded', ready());
+} else {
+    ready();
+}
 
-//lose screen if time runs out
+
+
+// card flip on mouse click
+
+// count number of flips
+
+// keep flipped if match
+
+// flip back over if not a match
+
